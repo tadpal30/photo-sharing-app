fc6c9fdc66f011b5fd960fdc84cf6ce85668b643
Tadeo Palafox <palafot@miamioh.edu>
2021-11-07 14:21:12 -0500

    Created and set up project

---
+++ src/auth/index.js
@@ -0,0 +1,2 @@
+export { PrivateRoute } from './PrivateRoute';
+export { useUser } from './useUser';
--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB7_Ec4jgQyO7CylhCr-SPAUDj7tPg9vYk",
+  authDomain: "photo-sharing-app-4ca35.firebaseapp.com",
+  projectId: "photo-sharing-app-4ca35",
+  storageBucket: "photo-sharing-app-4ca35.appspot.com",
+  messagingSenderId: "201310600633",
+  appId: "1:201310600633:web:d4762e38c37635f8e6ca68",
+  measurementId: "G-8PYTH7NT10"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
